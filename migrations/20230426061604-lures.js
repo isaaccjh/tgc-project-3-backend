8d62b6c77021b404eee86a3b11cdcf839a5a99da
Isaac <isaacforschool05@gmail.com>
2023-04-26 21:56:40 +0800

    created migration file for lures table

---
+++ migrations/20230426061604-lures.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable("lures", {
+    id: {
+      type: "int",
+      unsigned: true,
+      notNull: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: "string",
+      length: 100,
+      notNull: true
+    },
+    description: {
+      type: "text",
+      notNull: true
+    },
+    hook: {
+      type: "string",
+      length: 5,
+      notNull: true
+    },
+    type: {
+      type: "string",
+      length: 20,
+      notNull: true
+    },
+    size: {
+      type: "decimal",
+      unsigned: true,
+      notNull: true
+    },
+    weight: {
+      type: "decimal",
+      unsigned: true,
+      notNull: true
+    },
+    depth: {
+      type: "decimal",
+      unsigned: true
+    },
+    image_url: {
+      type: "string",
+      length: 2048,
+      notNull: true
+    },
+    thumbnail_url: {
+      type: "string",
+      length: 2048,
+      notNull: true
+    }
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable("lures");
+};
+
+exports._meta = {
+  "version": 1
+};
--- routes/lures.js
+++ routes/lures.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("lures/index");
-})
-
+    res.render("lures/index")
+});
 
 module.exports = router;
