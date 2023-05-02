9a87186ec962d439dbd3748c127c19f10e2082bc
Isaac <isaacforschool05@gmail.com>
2023-05-02 11:19:09 +0800

    render order/index.hbs

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const orderDataLayer = require("../dal/orders")
+const orderDataLayer = require("../dal/orders");
 
 router.get("/", async (req, res) => {
     const orders = await orderDataLayer.getAllOrders();
-    console.log(orders.toJSON())
+    console.log(orders.toJSON());
+    res.render("orders/index", {
+        orders: orders.toJSON()
+    })
 })
 
 module.exports = router;
