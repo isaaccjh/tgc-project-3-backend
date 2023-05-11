ea827dddcf9c850f77e5a83058cba7458e0dcae8
Isaac <isaacforschool05@gmail.com>
2023-05-11 14:35:33 +0800

    added delete route for cart

--- routes/api/carts.js
+++ routes/api/carts.js
@@ -18,9 +18,17 @@ router.post("/update", async (req, res) => {
         const cart = await cartDataLayer.updateQuantity(req.body.userId, req.body.variantId, req.body.quantity);
         res.status(200).send(cart)
     } catch (e) {
-        res.status(404).send("Failed");
+        res.status(404).send("Failed to update, please try again later.");
+    }  
+})
+
+router.post("/delete", async (req, res) => {
+    try {
+        const cart = await cartDataLayer.removeFromCart(req.body.userId, req.body.variantId);
+        res.status(200).send("Successfully removed from cart!")
+    } catch (e) {
+        res.status(404).send("Failed to delete, please try again later.")
     }
-    
 })
 
 module.exports = router;
