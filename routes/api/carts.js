a484193a2929095b3aac2803dd7f229f31fcc06b
Isaac <isaacforschool05@gmail.com>
2023-05-11 12:23:18 +0800

    added update route and middleware to carts.js api

--- routes/api/carts.js
+++ routes/api/carts.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const cartDataLayer = require("../../dal/cart_items");
+const { checkIfAuthenticatedJWT } = require("../../middlewares")
 
-router.get("/:user_id", async (req, res) => {
-    const cart = await cartDataLayer.getCart(req.params.user_id);
-    res.send(cart);
+router.get("/:user_id", checkIfAuthenticatedJWT, async (req, res) => {
+    try {
+        const cart = await cartDataLayer.getCart(req.params.user_id);
+        res.status(200).send(cart);
+    } catch (e) {
+        res.sendStatus(404);
+    }
+})
+
+router.post("/:variant_id/update", checkIfAuthenticatedJWT, async (req, res) => {
+    try {
+        const cart = await cartDataLayer.updateQuantity(req.body.userId, req.params.variant_id, req.body.quantity);
+        res.status(200).send("Updated", cart)
+    } catch (e) {
+        res.sendStatus(404);
+    }
+    
 })
 
 module.exports = router;
