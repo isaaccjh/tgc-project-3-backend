d62c54a7a5f24d09fb068426fa40167857b88ea8
Isaac <isaacforschool05@gmail.com>
2023-05-11 10:33:33 +0800

    typo(?) changed series to serie in getCart function

--- dal/cart_items.js
+++ dal/cart_items.js
@@ -5,7 +5,7 @@ const getCart = async (userId) => {
         "user_id": userId
     }).fetch({
         require: false,
-        withRelated: ["variant", "variant.lure", "variant.colour", "variant.series", "variant.property"]
+        withRelated: ["variant", "variant.lure", "variant.colour", "variant.serie", "variant.property"]
     });
     return cart;
 }
