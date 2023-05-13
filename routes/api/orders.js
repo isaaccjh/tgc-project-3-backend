1b47fdc3773186dc99974b1accc75c1c8fe3d2d7
Isaac <isaacforschool05@gmail.com>
2023-05-13 20:44:20 +0800

    created get order by user id route in orders api

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const orderDataLayer = require("../../dal/orders")
+
+router.get("/:user_id", async (req, res) => {
+    try {
+        const orders = await orderDataLayer.getOrdersByUserId(req.params.user_id);
+        res.status(200).send(orders);
+    } catch (e) {
+        res.status(404).send("No order found")
+    }   
+});
 
 
 module.exports = router;
