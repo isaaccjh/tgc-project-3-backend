53ece40222cab9a30a633310325195b6877d66fa
Isaac <isaacforschool05@gmail.com>
2023-05-15 10:47:02 +0800

    added new migration to add admin value to roles

--- routes/orders.js
+++ routes/orders.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-import { checkIfAuthenticated } from "../middlewares";
+import { checkIfAdmin, checkIfAuthenticated } from "../middlewares";
 
 const orderDataLayer = require("../dal/orders");
 
-router.get("/", async (req, res) => {
+router.get("/", checkIfAdmin, async (req, res) => {
     const orders = await orderDataLayer.getAllOrders();
     res.render("orders/index", {
         orders: orders.toJSON()
     })
 })
 
-router.get("/:order_id", async (req, res) => {
+router.get("/:order_id", checkIfAdmin, async (req, res) => {
     const orders = await orderDataLayer.getOrderItemsByOrderId(req.params.order_id);
 
     res.render("orders/details", {
