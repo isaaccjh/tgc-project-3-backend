35baeff1adbc53b4a2e2a2012d06c7ef5931cc76
Isaac <isaacforschool05@gmail.com>
2023-05-15 10:41:12 +0800

    imported checkIfAuthenticated middleware to orders routeg

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+import { checkIfAuthenticated } from "../middlewares";
+
 const orderDataLayer = require("../dal/orders");
 
 router.get("/", async (req, res) => {
