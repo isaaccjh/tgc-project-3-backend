21bdfe513bea58ad84904ee97f01269ad71098e0
Isaac <isaacforschool05@gmail.com>
2023-05-15 10:48:46 +0800

    added middleware into all shoppingCart routes

--- routes/shoppingCart.js
+++ routes/shoppingCart.js
@@ -6,7 +6,7 @@ import { checkIfAuthenticated } from "../middlewares";
 const CartServices = require("../services/cart_services");
 const lureDataLayer = require("../dal/lures");
 
-router.get("/", async (req, res) => {
+router.get("/", checkIfAuthenticated, async (req, res) => {
     let cart = new CartServices(req.session.user.id);
     const getCart = (await cart.getCart()).toJSON()
     res.render("carts/index", {
@@ -14,21 +14,21 @@ router.get("/", async (req, res) => {
     })
 })
 
-router.get("/variant/:variant_id/add", async (req, res) => {
+router.get("/variant/:variant_id/add", checkIfAuthenticated, async (req, res) => {
     let cart = new CartServices(req.session.user.id);
     await cart.addToCart(req.params.variant_id, 1);
     req.flash("success_messages", "Added to cart");
     res.redirect("/lures")
 })
 
-router.get("/variant/:variant_id/remove", async (req, res) => {
+router.get("/variant/:variant_id/remove", checkIfAuthenticated, async (req, res) => {
     let cart = new CartServices(req.session.user.id);
     await cart.remove(req.params.variant_id);
     req.flash("success_messages", "Item has been removed");
     res.redirect("/cart");
 });
 
-router.post("/variant/:variant_id/quantity/update", async (req, res) => {
+router.post("/variant/:variant_id/quantity/update", checkIfAuthenticated, async (req, res) => {
     let cart = new CartServices(req.session.user.id);
     await cart.setQuantity(req.params.variant_id, req.body.newQuantity);
     req.flash("success_messages", "Quantity Updated");
