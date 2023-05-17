b50f05af9fb3104e298367699528b53738437800
Isaac <isaacforschool05@gmail.com>
2023-05-17 22:57:46 +0800

    might have found the error

--- dal/cart_items.js
+++ dal/cart_items.js
@@ -1,32 +1,42 @@
 const { CartItem } = require("../models");
 
 const getCart = async (userId) => {
-    const cart = await CartItem.collection()
-        .where({
-            user_id: userId,
-        })
-        .fetch({
-            require: false,
-            withRelated: [
-                "variant",
-                "variant.lure",
-                "variant.colour",
-                "variant.property",
-                "lure",
-                "lure.serie",
-            ],
-        });
-    return cart;
+    try {
+        const cart = await CartItem.collection()
+            .where({
+                user_id: userId,
+            })
+            .fetch({
+                require: false,
+                withRelated: [
+                    "variant",
+                    "variant.lure",
+                    "variant.colour",
+                    "variant.property",
+                    "lure",
+                    "lure.serie",
+                ],
+            });
+        return cart;
+    } catch (e) {
+        console.log(e)
+        return false;
+    }
 };
 
 const getCartItemByUserAndVariant = async (userId, variantId) => {
-    const items = await CartItem.where({
-        user_id: userId,
-        variant_id: variantId,
-    }).fetch({
-        require: false,
-    });
-    return items;
+    try {
+        const items = await CartItem.where({
+            user_id: userId,
+            variant_id: variantId,
+        }).fetch({
+            require: false,
+        });
+        return items;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
 };
 
 const createCartItem = async (userId, variantId, quantity) => {
@@ -40,12 +50,17 @@ const createCartItem = async (userId, variantId, quantity) => {
 };
 
 const removeFromCart = async (userId, variantId) => {
-    let cartItem = await getCartItemByUserAndVariant(userId, variantId);
-    if (cartItem) {
-        await cartItem.destroy();
-        return true;
+    try {
+        let cartItem = await getCartItemByUserAndVariant(userId, variantId);
+        if (cartItem) {
+            await cartItem.destroy();
+            return true;
+        }
+        return false;
+    } catch (e) {
+        console.log(e);
+        return false;
     }
-    return false;
 };
 
 const updateQuantity = async (userId, variantId, newQuantity) => {
--- routes/api/checkout.js
+++ routes/api/checkout.js
@@ -8,152 +8,152 @@ const { getOrderData } = require("../../helpers/getOrderData");
 const orderDataLayer = require("../../dal/orders");
 const cartDataLayer = require("../../dal/cart_items");
 
-router.get("/:user_id", express.json(), async (req, res) => {
-  const cart = new CartServices(req.params.user_id);
-  let items = await cart.getCart();
+router.get("/", [express.json(), checkIfAuthenticated], async (req, res) => {
+    const cart = new CartServices(req.session.user.id);
+    let items = await cart.getCart();
 
-  let lineItems = [];
-  let meta = [];
+    let lineItems = [];
+    let meta = [];
 
-  for (let i of items) {
-    const lineItem = {
-      quantity: i.get("quantity"),
-      price_data: {
-        currency: "sgd",
-        unit_amount: i.related("variant").get("cost"),
-        product_data: {
-          name: i.related("variant").toJSON().lure.name,
-        },
-      },
-    };
-    if (i.related("variant").get("image_url")) {
-      lineItem.price_data.product_data.images = [
-        i.related("variant").get("image_url"),
-      ];
+    for (let i of items) {
+        const lineItem = {
+            quantity: i.get("quantity"),
+            price_data: {
+                currency: "sgd",
+                unit_amount: i.related("variant").get("cost"),
+                product_data: {
+                    name: i.related("variant").toJSON().lure.name,
+                },
+            },
+        };
+        if (i.related("variant").get("image_url")) {
+            lineItem.price_data.product_data.images = [
+                i.related("variant").get("image_url"),
+            ];
+        }
+        lineItems.push(lineItem);
+        meta.push({
+            user_id: req.session.user.id,
+            variant_id: i.get("variant_id"),
+            quantity: i.get("quantity"),
+        });
     }
-    lineItems.push(lineItem);
-    meta.push({
-      user_id: req.params.user_id,
-      variant_id: i.get("variant_id"),
-      quantity: i.get("quantity"),
+
+    let metaData = JSON.stringify(meta);
+    const payment = {
+        payment_method_types: ["card", "grabpay"],
+        mode: "payment",
+        line_items: lineItems,
+        success_url:
+            process.env.STRIPE_SUCCESS_URL + "?sessionId={CHECKOUT_SESSION_ID}",
+        cancel_url: process.env.STRIPE_ERROR_URL,
+        metadata: {
+            orders: metaData,
+        },
+        shipping_address_collection: {
+            allowed_countries: ["SG"],
+        },
+        billing_address_collection: "required",
+        invoice_creation: {
+            enabled: true,
+        },
+        shipping_options: [
+            {
+                shipping_rate_data: {
+                    display_name: "Standard",
+                    type: "fixed_amount",
+                    fixed_amount: {
+                        amount: 300,
+                        currency: "sgd",
+                    },
+                    delivery_estimate: {
+                        minimum: {
+                            unit: "business_day",
+                            value: 4,
+                        },
+                        maximum: {
+                            unit: "business_day",
+                            value: 7,
+                        },
+                    },
+                },
+            },
+            {
+                shipping_rate_data: {
+                    display_name: "Express",
+                    type: "fixed_amount",
+                    fixed_amount: {
+                        amount: 500,
+                        currency: "sgd",
+                    },
+                    delivery_estimate: {
+                        minimum: {
+                            unit: "business_day",
+                            value: 1,
+                        },
+                        maximum: {
+                            unit: "business_day",
+                            value: 3,
+                        },
+                    },
+                },
+            },
+        ],
+    };
+
+
+    let stripeSession = await Stripe.checkout.sessions.create(payment);
+    res.render("checkout/checkout", {
+        sessionId: stripeSession.id,
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     });
-  }
-
-  let metaData = JSON.stringify(meta);
-  const payment = {
-    payment_method_types: ["card", "grabpay"],
-    mode: "payment",
-    line_items: lineItems,
-    success_url:
-      process.env.STRIPE_SUCCESS_URL + "?sessionId={CHECKOUT_SESSION_ID}",
-    cancel_url: process.env.STRIPE_ERROR_URL,
-    metadata: {
-      orders: metaData,
-    },
-    shipping_address_collection: {
-      allowed_countries: ["SG"],
-    },
-    billing_address_collection: "required",
-    invoice_creation: {
-      enabled: true,
-    },
-    shipping_options: [
-      {
-        shipping_rate_data: {
-          display_name: "Standard",
-          type: "fixed_amount",
-          fixed_amount: {
-            amount: 300,
-            currency: "sgd",
-          },
-          delivery_estimate: {
-            minimum: {
-              unit: "business_day",
-              value: 4,
-            },
-            maximum: {
-              unit: "business_day",
-              value: 7,
-            },
-          },
-        },
-      },
-      {
-        shipping_rate_data: {
-          display_name: "Express",
-          type: "fixed_amount",
-          fixed_amount: {
-            amount: 500,
-            currency: "sgd",
-          },
-          delivery_estimate: {
-            minimum: {
-              unit: "business_day",
-              value: 1,
-            },
-            maximum: {
-              unit: "business_day",
-              value: 3,
-            },
-          },
-        },
-      },
-    ],
-  };
-
-  console.log(lineItems);
-  let stripeSession = await Stripe.checkout.sessions.create(payment);
-  res.render("checkout/checkout", {
-    sessionId: stripeSession.id,
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
 });
 
 router.post(
-  "/process_payment",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    let payload = req.body;
-    let endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
-    let sigHeader = req.headers["stripe-signature"];
-    let event;
-    try {
-      event = Stripe.webhooks.constructEvent(
-        payload,
-        sigHeader,
-        endpointSecret
-      );
-    } catch (e) {
-      res.send({
-        error: e.message,
-      });
-      console.log(e.message);
+    "/process_payment",
+    express.raw({ type: "application/json" }),
+    async (req, res) => {
+        let payload = req.body;
+        let endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+        let sigHeader = req.headers["stripe-signature"];
+        let event;
+        try {
+            event = Stripe.webhooks.constructEvent(
+                payload,
+                sigHeader,
+                endpointSecret
+            );
+        } catch (e) {
+            res.send({
+                error: e.message,
+            });
+            console.log(e.message);
+        }
+        if (event.type == "checkout.session.completed") {
+            let stripeSession = event.data.object;
+            const paymentIntent = await Stripe.paymentIntents.retrieve(
+                stripeSession.payment_intent
+            );
+
+            // console.log(JSON.parse(stripeSession.metadata.orders))
+            const userId = JSON.parse(stripeSession.metadata.orders)[0].user_id;
+
+            let orderData = getOrderData(userId, stripeSession, paymentIntent);
+
+            const orderItems = JSON.parse(stripeSession.metadata.orders);
+
+            const newOrder = await orderDataLayer.addOrder(orderData);
+
+            const order = await orderDataLayer.findOrderByStripeId(stripeSession.id);
+
+            orderItems.forEach(async (item) => {
+                await orderDataLayer.addOrderItem(order.id, item.variant_id, item.quantity);
+            });
+            // after checking out, clear cart items from user
+            await cartDataLayer.clearUserCart(userId);
+        }
+        res.send({ received: true });
     }
-    if (event.type == "checkout.session.completed") {
-      let stripeSession = event.data.object;
-      const paymentIntent = await Stripe.paymentIntents.retrieve(
-        stripeSession.payment_intent
-      );
-
-      // console.log(JSON.parse(stripeSession.metadata.orders))
-      const userId = JSON.parse(stripeSession.metadata.orders)[0].user_id;
-
-      let orderData = getOrderData(userId, stripeSession, paymentIntent);
-
-      const orderItems = JSON.parse(stripeSession.metadata.orders);
-
-      const newOrder = await orderDataLayer.addOrder(orderData);
-
-      const order = await orderDataLayer.findOrderByStripeId(stripeSession.id);
-
-      orderItems.forEach((item) => {
-        orderDataLayer.addOrderItem(order.id, item.variant_id, item.quantity);
-      });
-      // after checking out, clear cart items from user
-      await cartDataLayer.clearUserCart(userId);
-    }
-    res.send({ received: true });
-  }
 );
 
 module.exports = router;
