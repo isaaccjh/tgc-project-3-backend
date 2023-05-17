d6e670e2aa6681584499ad25844d0ac33172e41d
Isaac <isaacforschool05@gmail.com>
2023-05-17 22:45:03 +0800

    added try catch to some cart routes

--- dal/cart_items.js
+++ dal/cart_items.js
@@ -1,81 +1,85 @@
 const { CartItem } = require("../models");
 
 const getCart = async (userId) => {
-  const cart = await CartItem.collection()
-    .where({
-      user_id: userId,
-    })
-    .fetch({
-      require: false,
-      withRelated: [
-        "variant",
-        "variant.lure",
-        "variant.colour",
-        "variant.property",
-        "lure",
-        "lure.serie",
-      ],
-    });
-  return cart;
+    const cart = await CartItem.collection()
+        .where({
+            user_id: userId,
+        })
+        .fetch({
+            require: false,
+            withRelated: [
+                "variant",
+                "variant.lure",
+                "variant.colour",
+                "variant.property",
+                "lure",
+                "lure.serie",
+            ],
+        });
+    return cart;
 };
 
 const getCartItemByUserAndVariant = async (userId, variantId) => {
-  const items = await CartItem.where({
-    user_id: userId,
-    variant_id: variantId,
-  }).fetch({
-    require: false,
-  });
-  return items;
+    const items = await CartItem.where({
+        user_id: userId,
+        variant_id: variantId,
+    }).fetch({
+        require: false,
+    });
+    return items;
 };
 
 const createCartItem = async (userId, variantId, quantity) => {
-  let cartItem = new CartItem({
-    user_id: userId,
-    variant_id: variantId,
-    quantity: quantity,
-  });
-  await cartItem.save();
-  return cartItem;
+    let cartItem = new CartItem({
+        user_id: userId,
+        variant_id: variantId,
+        quantity: quantity,
+    });
+    await cartItem.save();
+    return cartItem;
 };
 
 const removeFromCart = async (userId, variantId) => {
-  let cartItem = await getCartItemByUserAndVariant(userId, variantId);
-  if (cartItem) {
-    await cartItem.destroy();
-    return true;
-  }
-  return false;
+    let cartItem = await getCartItemByUserAndVariant(userId, variantId);
+    if (cartItem) {
+        await cartItem.destroy();
+        return true;
+    }
+    return false;
 };
 
 const updateQuantity = async (userId, variantId, newQuantity) => {
-  let cartItem = await getCartItemByUserAndVariant(userId, variantId);
-  if (cartItem) {
-    cartItem.set("quantity", newQuantity);
-    cartItem.save();
-    return true;
-  }
-  return false;
+    let cartItem = await getCartItemByUserAndVariant(userId, variantId);
+    if (cartItem) {
+        cartItem.set("quantity", newQuantity);
+        cartItem.save();
+        return true;
+    }
+    return false;
 };
 
 const clearUserCart = async (userId) => {
-  let userCart = await getCart(userId);
-  // running through the cart to clear all of them
-  if (userCart) {
-    userCart.forEach(async (item) => {
-      console.log("destroying!");
-      await item.destroy();
-    });
-    return true;
-  }
-  return false;
+    try {
+        let userCart = await getCart(userId);
+        // running through the cart to clear all of them
+        if (userCart) {
+            userCart.forEach(async (item) => {
+                console.log("destroying!");
+                await item.destroy();
+            });
+            return true;
+        }
+        return false;
+    } catch (e) {
+        console.log(e)
+    }
 };
 
 module.exports = {
-  getCart,
-  getCartItemByUserAndVariant,
-  createCartItem,
-  removeFromCart,
-  updateQuantity,
-  clearUserCart,
+    getCart,
+    getCartItemByUserAndVariant,
+    createCartItem,
+    removeFromCart,
+    updateQuantity,
+    clearUserCart,
 };
